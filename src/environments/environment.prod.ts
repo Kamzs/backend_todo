5a76e42955f47e7b0d4a3c8c22cf643a60333f34
k.ambroziak <k.ambroziak@samsung.com>
2020-03-18 15:27:57 +0100

    save

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBzKILwvkvu5Ei2yD7icB7e4vOx8Yhc_IM",
+    authDomain: "autoryzacjatodo.firebaseapp.com",
+    databaseURL: "https://autoryzacjatodo.firebaseio.com",
+    projectId: "autoryzacjatodo",
+    storageBucket: "autoryzacjatodo.appspot.com",
+    messagingSenderId: "939380083242",
+    appId: "1:939380083242:web:748d971412922495195153"
+  }
 };
